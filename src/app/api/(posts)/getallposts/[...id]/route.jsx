e10be9984bfaeb7d4b24543a0303a63bc0f1b09f
unknown <wildgtr@gmail.com>
2024-03-12 08:38:29 -0400

    'test'

--- src/app/api/(posts)/getallposts/[...id]/route.jsx
+++ src/app/api/(posts)/getallposts/[...id]/route.jsx
@@ -2,6 +2,7 @@
 
 import dbConnect from '@/app/util/DBConnect';
 import Post from '@/app/models/post'
+import Comment from '@/app/models/comment'
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req) {
@@ -10,10 +11,24 @@ export async function GET(req) {
   let userId = params.substr(-24)
 
   await dbConnect();
-
-  let allPosts = await Post.find()
-
+  
   // let foundArticle = await Article.findOne({_id: id }).populate('comments').exec()
+
+  // let allPosts = await Post.find().sort({updatedAt: 1}).populate(
+  // let allPosts = await Post.find().populate('comments.body').exec()
+  let allPosts = await Post.aggregate([{ 
+    $lookup:
+    {
+      from: 'comments',
+      localField: '_id',
+      foreignField: 'post',
+      as: 'replies'
+    }
+  }])
+
+  if (allPosts) {
+    console.log(allPosts)
+  }
 
   if (allPosts) {
     return NextResponse.json({message: allPosts}, { status: 200 })
--- src/app/models/comment.js
+++ src/app/models/comment.js
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
 
-const commentSchema = new Schema({
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Post',
-  },
+const commentSchema = new Schema(
+  {
+    body: String,
 
-  comment: {
-    type: String,
-  },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+    },
 
-  userid: {
-    type: String,
-  }
-}, {timestamps: true})
+    userid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  }, {timestamps: true})
 
 const Comment = mongoose.models.Comment || mongoose.model("Comment", commentSchema)
 export default Comment
