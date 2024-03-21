946ecfa4aa2f144db8123c56268ef915782df9e9
YK <young@ykstudio.dev>
2024-03-21 10:34:04 -0400

    form validation done

--- src/app/(pages)/channel/[[...slug]]/components/addChannelModal.jsx
+++ src/app/(pages)/channel/[[...slug]]/components/addChannelModal.jsx
@@ -1,5 +1,5 @@
-import { Dialog, Transition } from '@headlessui/react'
-import { MdClose, MdOutlineCircle, MdOutlineCheckCircle, MdCoPresent, MdOutlineGroup, MdPeopleAlt } from 'react-icons/md'
+import { Dialog, Transition, Switch } from '@headlessui/react'
+import { MdClose, MdOutlineCircle, MdOutlineCheckCircle, MdCoPresent, MdPeopleAlt, MdLock, MdCheckCircle } from 'react-icons/md'
 import { FaHashtag } from "react-icons/fa6";
 import { Fragment, useState } from 'react'
 import axios from 'axios'
@@ -8,10 +8,119 @@ import { motion } from 'framer-motion'
 const channelTypes = [
   { type: 'Text' },
   { type: 'Collaboration' },
-  { type: 'Viewer Participation' },
+  { type: 'Participation' },
 ]
 
+// 투표
+// 토토
+// 미션 socket 필수
+// 도네
+// 돌림판
+
+const channelTypeDistributor = (type, selected) => {
+  if(type === 'Text') {
+    return (
+      <div className={selected === 'Text' ? 'flex flex-row justify-between items-center bg-black/30 px-3 py-2 rounded-md' : 'flex flex-row justify-between items-center bg-black/10 hover:bg-white/10 hover:cursor-pointer px-3 py-2 rounded-md'}>
+        <div className='flex flex-row gap-2 items-center'>
+          <FaHashtag className='w-5 h-5'/>
+          <div className='text-sm'>
+            <p className='font-medium'>Text Channel</p>
+            <p className='text-slate-300 text-xs'>Send messages, images, GIFs, share opinions</p>
+          </div>
+        </div>
+        {selected === 'Text' ? <MdCheckCircle className='w-5 h-5 text-sky-500' /> : <MdOutlineCircle className='w-5 h-5' />}
+      </div>
+    )
+  }
+
+  if(type === 'Collaboration') {
+    return (
+      <div className={selected === 'Collaboration' ? 'flex flex-row justify-between items-center bg-black/30 px-3 py-2 rounded-md' : 'flex flex-row justify-between items-center bg-black/10 hover:bg-white/10 hover:cursor-pointer px-3 py-2 rounded-md'}>
+        <div className='flex flex-row gap-2 items-center'>
+          <MdPeopleAlt className='w-5 h-5'/>
+          <div className='text-sm'>
+            <p className='font-medium'>Collaboration Channel</p>
+            <p className='text-slate-300 text-xs'>Connect and schedule with other streamers</p>
+          </div>
+        </div>
+        {selected === 'Collaboration' ? <MdCheckCircle className='w-5 h-5 text-sky-500' /> : <MdOutlineCircle className='w-5 h-5' />}
+      </div>
+    )
+  }
+
+  if(type === 'Participation') {
+    return (
+      <div className={selected === 'Participation' ? 'flex flex-row justify-between items-center bg-black/30 px-3 py-2 rounded-md' : 'flex flex-row justify-between items-center bg-black/10 hover:bg-white/10 hover:cursor-pointer px-3 py-2 rounded-md'}>
+        <div className='flex flex-row gap-2 items-center'>
+          <MdCoPresent className='w-5 h-5'/>
+          <div className='text-sm'>
+            <p className='font-medium'>Participation Channel</p>
+            <p className='text-slate-300 text-xs'>Connect and schedule with stream viewers</p>
+          </div>
+        </div>
+        {selected === 'Participation' ? <MdCheckCircle className='w-5 h-5 text-sky-500' /> : <MdOutlineCircle className='w-5 h-5' />}
+      </div>
+    )
+  }
+
+}
+
 const AddChannelModal = ({isModalOpen, setIsModalOpen, channelUser}) => {
+
+  const [ channelSubmitForm, setChannelSubmitForm ] = useState(
+    {
+      channelName: '',
+      author: '',
+      isPrivate: false,
+      channelType: 'Text'
+    }
+  )
+
+  const { channelName, author, isPrivate, channelType } = channelSubmitForm
+
+  const channelTypeButtonHandler = (e, type) => {
+    if(type === channelType) {
+      return
+    }
+
+    setChannelSubmitForm((prev) => ({
+      ...prev,
+      channelType: type
+    }))
+  }
+
+  const channelNameChangeHandler = (e) => {
+    setChannelSubmitForm((prev) => ({
+      ...prev,
+      channelName: e.target.value
+    }))
+  }
+
+  const channelIsPrivateChangeHandler = (e, state) => {
+    setChannelSubmitForm((prev) => ({
+    ...prev,
+      isPrivate: !state
+    }))
+  }
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  const submitHandler = (e) => {
+    // validate the form
+    const specialCharacters = `/[!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?]+/ `
+    const notValidated = specialCharacters.split('').some(char => channelName.includes(char)) 
+    const numberOfCharacters = () => {
+      if(channelName.length > 2 && channelName.length < 16) {
+        return true
+      } else {
+        return false
+      }
+    }
+    console.log('clicked', numberOfCharacters())
+  }
+
   return (
     <Transition.Root show={isModalOpen} as={Fragment}>
       <Dialog as='div' className='relative z-20' onClose={setIsModalOpen}>
@@ -38,7 +147,7 @@ const AddChannelModal = ({isModalOpen, setIsModalOpen, channelUser}) => {
             >
               <Dialog.Panel className='flex w-full transform transition max-w-sm text-base'>
                 <motion.div
-                  className='relative w-full overflow-hidden bg-slate-700 shadow-2xl p-6 rounded-lg'
+                  className='relative w-full overflow-hidden bg-slate-700 shadow-2xl px-4 py-6 rounded-lg'
                   initial={{opacity: 0}}
                   animate={{opacity: 1}}
                   transition={{duration: 0.45}}
@@ -53,25 +162,85 @@ const AddChannelModal = ({isModalOpen, setIsModalOpen, channelUser}) => {
                     </div>
 
                     {/* channel types */}
-                    <div>
-                      <p className='text-xs font-bold'>Channel type</p>
+                    <div className='flex flex-col gap-1'>
+                      <p className='text-xs font-bold pb-1'>Channel Type</p>
                       {channelTypes.map((channel) =>(
                         <div
-                          key={channel.type + 'modalChannel'}
-                          className='flex flex-row items-center gap-2'
+                        key={channel.type + 'modalChannel'}
+                        onClick={(e) => channelTypeButtonHandler(e, channel.type)}
                         >
-
+                          {channelTypeDistributor(channel.type, channelType)}
                         </div>
                       ))}
-                      <div className='flex flex-row items-center gap-2'>
-                        <FaHashtag />
-                        <div className='text-sm'>
-                          <p className='font-bold'>Text Channel</p>
-                          <p className='text-slate-300 text-xs'>Send messages, images, GIFs, share opinions</p>
-                        </div>
-                      </div>
                     </div>
 
+                    {/* channel name */}
+                    <div className='flex flex-col gap-1'>
+                      <div className='pb-1'>
+                        <p className='text-xs font-bold'>Channel Name</p>
+                        <p className='text-slate-300 text-xs'>Special charactors and spaces are not allowed, 2-16 letters</p>
+                      </div>
+                      <input 
+                        type='text' 
+                        value={channelName} 
+                        onChange={channelNameChangeHandler} 
+                        placeholder='Enter channel name'
+                        className='bg-black/30 ring-0 border-none rounded-md focus:ring-0 text-sm px-4 py-2'
+                      />
+                    </div>
+
+                    {/* private option */}
+                    <div className='flex flex-row justify-between items-center gap-1'>
+                      <div className='flex flex-row gap-2 items-center'>
+                        <MdLock className='w-5 h-5'/>
+                        <div>
+                          <p className='text-xs font-bold'>Private Channel</p>
+                          <p className='text-slate-300 text-xs'>Only allowed ones will be able to view channel</p>
+                        </div>
+                      </div>
+                      <Switch
+                        checked={isPrivate}
+                        onChange={(e) => channelIsPrivateChangeHandler(e, isPrivate)}
+                        className={classNames(
+                          isPrivate ? 'bg-sky-500' : 'bg-gray-200',
+                          'relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-0'
+                        )}
+                      >
+                        <span
+                          className={classNames(
+                            isPrivate ? 'translate-x-5' : 'translate-x-0',
+                            'pointer-events-none relative inline-block h-4 w-4 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                          )}
+                        >
+                          <span
+                            className={classNames(
+                              isPrivate ? 'opacity-0 duration-100 ease-out' : 'opacity-100 duration-200 ease-in',
+                              'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity'
+                            )}
+                          >
+                            <svg className="h-3 w-3 text-gray-400" fill="none" viewBox="0 0 12 12">
+                              <path
+                                d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                            </svg>
+                          </span>
+                        </span>
+                      </Switch>
+                    </div>
+
+                    {/* buttons */}
+                    <div className='flex flex-col gap-2 pt-2 text-sm'>
+                      {channelName !== '' ? 
+                        <button className='bg-sky-500 py-2 rounded-md' onClick={submitHandler}>Create Channel</button>
+                        :
+                        <button className='bg-sky-500/40 py-2 rounded-md text-slate-300 cursor-not-allowed'>Create Channel</button>
+                      }
+                      <button className='py-2 hover:text-red-600'>Cancel</button>
+                    </div>
                   </div>
                 </motion.div>
               </Dialog.Panel>
